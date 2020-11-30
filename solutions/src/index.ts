c789486b1a37b96c6e2952df37f19b1f8c2d731e
Michael Arnaldi <arnaldimichael@gmail.com>
2020-11-30 16:55:15 +0000

    update material

--- solutions/src/index.ts
+++ solutions/src/index.ts
@@ -1,2 +1,17 @@
-// istanbul ignore file
-console.log("ok");
+import { chainReader, of, add, logMessage } from "./Reader";
+
+const result = chainReader(
+  chainReader(of(0), (n) => add(n, 3)),
+  (n) => logMessage(`(${n})`)
+);
+
+result({
+  ConsoleService: {
+    log: (message) => () => {
+      console.log(message);
+    },
+  },
+  MathService: {
+    add: (x, y) => x + y,
+  },
+});
